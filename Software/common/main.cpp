02ba971eb826fd03c375c009059bdbc407c61c27
alexzhang13 <alex.lx.zhang@gmail.com>
2017-04-15 21:16:09 -0400

    Updated Pi File for Testing

--- Software/common/main.cpp
+++ Software/common/main.cpp
@@ -6,22 +6,22 @@
 #include "UartTx.h"
 
 int main(int argc,char **argv){
-#if 0
-    char buf[30];
+#if 1
+    char* curr_line;
     //SerialPort *port = new SerialPort("/dev/ttyUSB0",115200);
     SerialPort *port = new SerialPort("/dev/ttyAMA0",115200);
 
     // loopback
-    port->printf("hello world!\n");
-    port->fgets(buf,30);
+    //port->printf("hello world!\n");
+    port->fgetln(curr_line);
 
-    printf("%s\n",buf);
+    printf("%s\n", curr_line);
 #endif
 
     //UartRx *uartrx = new UartRx(port);
     //UartTx *uarttx = new UartTx(port);
-    UartRx *uartrx = new UartRx(0);
-    UartTx *uarttx = new UartTx(0);
+    //UartRx *uartrx = new UartRx(0);
+    //UartTx *uarttx = new UartTx(0);
 
-    while(1) sleep(100);
+    //while(1) sleep(100);
 }
