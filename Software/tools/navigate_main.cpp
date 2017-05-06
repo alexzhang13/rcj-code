7a5a3c38a1fa80bf4e3080b9e5ad5a61e16b88b1
alexzhang13 <alex.lx.zhang@gmail.com>
2017-05-05 23:04:58 -0400

    Commited Master

--- Software/tools/navigate_main.cpp
+++ Software/tools/navigate_main.cpp
@@ -11,8 +11,8 @@
 int main(int argc, char **argv)
 {
 	//int ret = greedy_dikjstra_test();
-	int ret = mapgen_test();
-	//int ret = navigation_simul_test();
+	//int ret = mapgen_test();
+	int ret = navigation_simul_test();
 	//int ret = testMapLoad();
 	return ret;
 }
@@ -79,7 +79,7 @@ int mapgen_test()
 	const float walls = 0.2f;
 	int32_t home_floor_num = 0;
 	int32_t floors = 2;
-	const char* out_dir = "C:/projects/StormingRobots2017/Data/map_data";
+	const char* out_dir = "C:/projects/StormingRobots2017/Data/map_data/";
 	const char* xmlname = "mazemap";
 	const char* xmlname_new = "mazemap_new";
 	// generate floor maps
@@ -112,7 +112,7 @@ int mapgen_test()
 int navigation_simul_test()
 {
 	int32_t i;
-	const char* in_dir = "C:/projects/StormingRobots2017/Data/map_data";
+	const char* in_dir = "C:/projects/StormingRobots2017/Data/map_data/";
 	const char* xmlname = "mazemap";
 	int32_t home_floor_num = 0;
 	MazeCell::NavDir heading = MazeCell::navNorth;
@@ -147,7 +147,7 @@ int navigation_simul_test()
 int testMapLoad()
 {
 	int32_t i;
-	const char* in_dir = "C:/projects/StormingRobots2017/Data/map_data";
+	const char* in_dir = "C:/projects/StormingRobots2017/Data/map_data/";
 	const char* xmlname = "mazemap_04272017";
 	int32_t home_floor_num = 0;
 	MazeCell::NavDir heading = MazeCell::navNorth;
