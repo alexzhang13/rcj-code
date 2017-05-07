ef8a8535edc8c54d9577dfe3a73e2c767067220d
alexzhang13 <alex.lx.zhang@gmail.com>
2017-05-06 22:14:08 -0400

    test2

--- Software/common/ARobot.cpp
+++ Software/common/ARobot.cpp
@@ -372,8 +372,9 @@ void ARobot::TurnDistance(int degrees, BotDir dir)
 void ARobot::StopTurn(BotDir dir)
 {
     size_t imu_list = imuDataList.size();
-    float currYaw = imuDataList[imu_list-10].m_yaw;
-    printf("%f\n", currYaw);
+    float currYaw = imuDataList[imu_list-1].m_yaw;
+    printf("%f hi\n", currYaw);
+    printf("hi");
     if(dir == RIGHT) {
         if(initialYaw >= 180.0f && currYaw < 180.0f) { //if robot crosses over from 180 to -180, direction switches
             currYaw += 360; //range fixing
