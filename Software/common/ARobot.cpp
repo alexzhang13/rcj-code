3c43826878345550e92c08263e633206b481ff3b
alexzhang13 <alex.lx.zhang@gmail.com>
2017-05-06 22:20:38 -0400

    Print pos exceeders as neg

--- Software/common/ARobot.cpp
+++ Software/common/ARobot.cpp
@@ -373,11 +373,11 @@ void ARobot::StopTurn(BotDir dir)
 {
     size_t imu_list = imuDataList.size();
     float currYaw = imuDataList[imu_list-1].m_yaw;
-    printf("%f\n", currYaw);
     if(dir == RIGHT) {
         if(initialYaw <= 180.0f && currYaw > 180.0f) { //if robot crosses over from 180 to -180, direction switches
             currYaw -= 360; //range fixing
         }
+        printf("%f\n", currYaw);
         if(currYaw <= toTurn) {
             printf("%f\n", initialYaw);
             char* i_command;
