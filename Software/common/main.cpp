a493500702046ac8233b0b97a47644df43714e77
alexzhang13 <alex.lx.zhang@gmail.com>
2017-05-06 21:43:50 -0400

    speed test

--- Software/common/main.cpp
+++ Software/common/main.cpp
@@ -60,7 +60,7 @@ int main(int argc,char **argv){
         }
     }*/
     sleep(1);
-    myRobot->TurnDistance(90, ARobot::RIGHT);
+    //myRobot->TurnDistance(90, ARobot::RIGHT);
 	int32_t c = 0;
     while(1) {
 		//printf("test %d \n", c);
