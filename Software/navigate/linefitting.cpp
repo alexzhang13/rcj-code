c2df4bcb95f9fc1ee789739f6dc845d665b948bf
David Zhang <david.zhang@sri.com>
2017-05-14 10:18:24 -0400

    line fitting updates

--- Software/navigate/linefitting.cpp
+++ Software/navigate/linefitting.cpp
@@ -7,11 +7,12 @@ LineFitAlgo::LineFitAlgo()
 	mLongThresh = 1300;
 	mAngleSep = 0;
 	m_SampleCnt = 0;
-	m_cur_angle = -1;
+	m_scan_angle = -1;
 	mDistOffset = 13.75f; // mm 
 	mAngleOffset = -1.5; //-1.8; // degrees
 	mEpsilon = 20.0;
-	mLineThresh = 25;
+	mLineThresh = 10; //25;
+	mAngleThresh = 35.0f;
 	mAvgPts.clear();
 	mFittedLines.clear();
 }
@@ -50,6 +51,15 @@ void LineFitAlgo::resetData()
 			mPts[i].pt[j].y = 0.0;
 		}
 	}
+
+	mAvgPts.clear();
+	mLines.clear();
+	std::map<int32_t, FittedLine>::iterator it; 
+	for(it = mFittedLines.begin(); it != mFittedLines.end(); it++) {
+		LineFitAlgo::FittedLine fl = it->second;
+		fl.pts.clear();
+	}
+	mFittedLines.clear();
 }
 
 void LineFitAlgo::update(int32_t half_samples, int32_t angle_separation)
@@ -74,14 +84,14 @@ bool LineFitAlgo::readData(TimeDist &td)
 	bool rotate_left = false;
 
 	int32_t angle = td.angle;
-	if(angle == m_cur_angle) // angle not updated
+	if(angle == m_scan_angle) // angle not updated
 		return false;
 	else {
-		if(angle > m_cur_angle)
+		if(angle > m_scan_angle)
 			rotate_right = true;
-		else if(angle < m_cur_angle)
+		else if(angle < m_scan_angle)
 			rotate_left = false;
-		m_cur_angle = angle;
+		m_scan_angle = angle;
 		m_SampleCnt++;
 	}
 
@@ -175,20 +185,40 @@ bool LineFitAlgo::readDataFile(const char *filename)
 		if(ret)
 			break;
 
+		MazeCell::Position_2D pos;
+		pos.x = pos.y = 0;
+		setRobotStatus(MazeCell::navNorth, pos);
 		parseData(mTimeDist_rr);
 		parseData(mTimeDist_rl);
 		convert2Vec();
 		lineFit();
-		printoutData();
+		updateCellConfigs();
+		// printoutData();
 		displayPoints();
 		mAvgPts.clear();
 		mLines.clear();
+		std::map<int32_t, FittedLine>::iterator it; 
+		for(it = mFittedLines.begin(); it != mFittedLines.end(); it++) {
+			LineFitAlgo::FittedLine fl = it->second;
+			fl.pts.clear();
+		}
 		mFittedLines.clear();
 	}
 
 	fclose(hf);
 	return true;
 }
+
+
+void LineFitAlgo::setRobotStatus(MazeCell::NavDir direction, MazeCell::Position_2D pos)
+{
+	mDetectedCells.direction = direction;
+	mDetectedCells.pos = pos;
+	mDetectedCells.global_angle = 0.0f;
+	mDetectedCells.local_cell_list.clear();
+	return;
+}
+
 
 bool LineFitAlgo::convert2Vec()
 {
@@ -247,19 +277,6 @@ bool LineFitAlgo::convert2Vec()
 	return true;
 }
 
-
-bool LineFitAlgo::filterImage()
-{
-
-	return true;
-}
-
-bool LineFitAlgo::edgeDetection()
-{
-
-	return true;
-}
-
 bool LineFitAlgo::lineFit()
 {
 	int32_t i, j;
@@ -283,14 +300,159 @@ bool LineFitAlgo::lineFit()
 		A = p1.x - p0.x;
 		B = p1.y - p0.y;
 		C = p1.x*p0.y - p1.y*p0.x;
+		r = sqrt(A*A + B*B);
+		mFittedLines[j].aline.k = B/A;
+		mFittedLines[j].aline.intercept = p0.y - mFittedLines[j].aline.k * p0.x;
+		mFittedLines[j].aline.local_angle = atan(mFittedLines[j].aline.k) * 180.0f /3.1415926f;
+		mFittedLines[j].aline.side = -1;
+		mFittedLines[j].aline.distance = fabs(C/r);
+		if(fabs(mFittedLines[j].aline.local_angle) < mAngleThresh || fabs(fabs(mFittedLines[j].aline.local_angle) - 180.0f) < mAngleThresh) {
+			if(mFittedLines[j].aline.intercept > 0.0f)
+				mFittedLines[j].aline.side = 0; // upward
+			else
+				mFittedLines[j].aline.side = 2; // downward
+		}
+		else if(fabs(fabs(mFittedLines[j].aline.local_angle) - 90.0f) < mAngleThresh) {
+			if(mFittedLines[j].aline.intercept/mFittedLines[j].aline.k > 0.0f)
+				mFittedLines[j].aline.side = 3; // left
+			else
+				mFittedLines[j].aline.side = 1; // right
+		}
 
 		for(i = 0; i < mAvgPts.size(); i++) {
 			m = mAvgPts[i].x;
 			n = mAvgPts[i].y;
-			r = sqrt(A*A + B*B);
 			dist = (B*m - A*n + C)/r;
 			if(abs(dist) < epsilon) {
-				mFittedLines[j].push_back(mAvgPts[i]);
+				mFittedLines[j].pts.push_back(mAvgPts[i]);
+			}
+		}
+		printf("j = %d, angle %f, dist = %f, side = %d, pts = %d\n", j, mFittedLines[j].aline.local_angle, mFittedLines[j].aline.distance, 
+			mFittedLines[j].aline.side, mFittedLines[j].pts.size());
+	}
+
+	return true;
+}
+
+// check if cell_list size is 0. If it is, no information is obtained.
+bool LineFitAlgo::updateCellConfigs()
+{
+	int32_t i, j;
+	cv::Point2f p0, p1;
+
+	float orient_x = 0.0f;
+	float orient_y = 0.0f;
+	float distance[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+	int32_t cnt[4] = {0,0,0,0};
+	std::vector<int32_t> wall_status[4];
+	int32_t adjusted_wall_status[4] = {-1, -1, -1, -1};
+
+	// no line detected
+	if(mLines.size() < 1)
+		return false;
+
+	for(i = 0; i < mLines.size()-1; i++) {
+		if(mFittedLines[i].pts.size() > mLineThresh) {
+			if(mFittedLines[i].aline.side == 0)  // horizontal up
+			{
+				orient_x+= mFittedLines[i].aline.local_angle;
+				distance[0] += mFittedLines[i].aline.distance;
+				cnt[0]++;
+			}
+			else if(mFittedLines[i].aline.side == 2) // horizontal down
+			{
+				orient_x+= mFittedLines[i].aline.local_angle;
+				distance[2] += mFittedLines[i].aline.distance;
+				cnt[2]++;
+			}
+			else if(mFittedLines[i].aline.side == 3) // vertical left
+			{
+				float angle = mFittedLines[i].aline.local_angle;
+				distance[3] += mFittedLines[i].aline.distance;
+				if(angle < 0.0f)
+					angle = 180.0f + angle;
+				orient_y += angle;
+				cnt[3]++;
+			}
+			else if(mFittedLines[i].aline.side == 1) // vertical right
+			{
+				float angle = mFittedLines[i].aline.local_angle;
+				distance[1] += mFittedLines[i].aline.distance;
+				if(angle < 0.0f)
+					angle = 180.0f + angle;
+				orient_y += angle;
+				cnt[1]++;
+			}
+		}
+	}
+
+	orient_x = orient_x/(float)(cnt[0] + cnt[2]);
+	orient_y = orient_y/(float)(cnt[1] + cnt[3]);
+	for(i = 0;  i < 4; i++)
+		distance[i] /= float(cnt[i]);
+	// we are in trouble, not sure what happened
+	if(fabs(fabs(orient_y - orient_x) - 90.0f) > 10.0f)
+		return false;
+
+	MazeCell curCell;
+	mDetectedCells.cur_cell.setCellGrid(mDetectedCells.pos.x, mDetectedCells.pos.y);
+	MazeCell::NavDir direct = mDetectedCells.direction; // global direction
+
+	for(i = 0; i < 4; i++) {
+		if(distance[i] > 0.0f) 
+		{
+			float a_dist = distance[i];
+			while(a_dist - curCell.getCellWidth() >=0.0f) {
+				wall_status[i].push_back(1);
+				a_dist-= curCell.getCellWidth();
+			}
+			wall_status[i].push_back(0);
+		}
+	}
+
+	for(i = 0; i < 4; i++) {
+		if(wall_status[i].size() > 0)
+			adjusted_wall_status[i] = wall_status[(i+(int32_t)direct)%4][0];
+	}
+
+	// current cell update
+	setWallProp(curCell, adjusted_wall_status);
+	mDetectedCells.cur_cell = curCell;
+
+	// local cell detection and update
+	for(i = 0; i < 4; i++) {
+		int j = 1;
+		adjusted_wall_status[0] = adjusted_wall_status[1] = -1;
+		adjusted_wall_status[2] = adjusted_wall_status[3] = -1;
+		if(wall_status[i].size() > 0) {
+			while(wall_status[i].size() > j) {
+				MazeCell newcell;
+				MazeCell::NavDir newdir = MazeCell::NavDir((i+(int32_t)direct)%4);
+				int32_t x, y;
+				switch(newdir) {
+				case MazeCell::navNorth:
+					adjusted_wall_status[0] = wall_status[i][j];
+					adjusted_wall_status[2] = wall_status[i][j-1];
+					y = mDetectedCells.pos.y + j;
+				break;
+				case MazeCell::navEast:
+					adjusted_wall_status[1] = wall_status[i][j];
+					adjusted_wall_status[3] = wall_status[i][j-1];
+					x = mDetectedCells.pos.x + j;
+				break;
+				case MazeCell::navSouth:
+					adjusted_wall_status[2] = wall_status[i][j];
+					adjusted_wall_status[0] = wall_status[i][j-1];
+					y = mDetectedCells.pos.y + j;
+				break;
+				case MazeCell::navWest:
+					adjusted_wall_status[3] = wall_status[i][j];
+					adjusted_wall_status[1] = wall_status[i][j-1];
+					x = mDetectedCells.pos.x + j;
+				break;
+
+				}
+				j++;
 			}
 		}
 	}
@@ -298,11 +460,41 @@ bool LineFitAlgo::lineFit()
 	return true;
 }
 
-bool LineFitAlgo::polygonFit()
+void LineFitAlgo::setWallProp(MazeCell &curCell, int32_t *wall_status)
 {
+	int32_t i;
 
-	return true;
+	for(i = MazeCell::navNorth; i < MazeCell::navDirections; i++) {
+		MazeCell::NavDir direct = (MazeCell::NavDir)i;
+		if(direct == MazeCell::navNorth) {
+			if(wall_status[direct] == 1)
+				curCell.setWallNorth(MazeCell::WallProp::MOpen);
+			else if(wall_status[direct] == 0)
+				curCell.setWallNorth(MazeCell::WallProp::MWall);
+		}
+		else if(direct == MazeCell::navEast) {
+			if(wall_status[direct] == 1)
+				curCell.setWallEast(MazeCell::WallProp::MOpen);
+			else if(wall_status[direct] == 0)
+				curCell.setWallEast(MazeCell::WallProp::MWall);
+		}
+		else if(direct == MazeCell::navSouth) {
+			if(wall_status[direct] == 1)
+				curCell.setWallSouth(MazeCell::WallProp::MOpen);
+			else if(wall_status[direct] == 0)
+				curCell.setWallSouth(MazeCell::WallProp::MWall);
+		}
+		else if(direct == MazeCell::navWest) {
+			if(wall_status[direct] == 1)
+				curCell.setWallWest(MazeCell::WallProp::MOpen);
+			else if(wall_status[direct] == 0)
+				curCell.setWallWest(MazeCell::WallProp::MWall);
+		}
+	}
+
+	return;
 }
+
 
 void LineFitAlgo::displayPoints()
 {
@@ -326,8 +518,8 @@ void LineFitAlgo::displayPoints()
 	}
 #else
 	for(i = 0; i < mLines.size()-1; i++) {
-		for(j = 0; j < mFittedLines[i].size(); j++) {
-			cv::Point center = cv::Point(mFittedLines[i][j].x + xoffset, h-(mFittedLines[i][j].y+yoffset));
+		for(j = 0; j < mFittedLines[i].pts.size(); j++) {
+			cv::Point center = cv::Point(mFittedLines[i].pts[j].x + xoffset, h-(mFittedLines[i].pts[j].y+yoffset));
 			cv::circle( mImage, center,3, cv::Scalar( (i*40)%255, 0, 255 ), thickness, lineType );
 		}
 	}
@@ -337,12 +529,16 @@ void LineFitAlgo::displayPoints()
 	// display lines
 	cv::Point2f p0, p1;
 	for(i = 0; i < mLines.size()-1; i++) {
-		if(mFittedLines[i].size() > mLineThresh) {
+		if(mFittedLines[i].pts.size() > mLineThresh) {
 			p0.x = mLines[i].x + xoffset;
 			p0.y = h-(mLines[i].y + yoffset);
 			p1.x = mLines[i+1].x + xoffset;
 			p1.y = h-(mLines[i+1].y + yoffset);
 			cv::line(mImage, p0, p1, cv::Scalar( 255, 0, 0 ), 1, 8);
+			printf("Line# = %d, angle %f, dist = %f, side = %d, pts = %d\n", i, mFittedLines[i].aline.local_angle, mFittedLines[i].aline.distance,
+				mFittedLines[i].aline.side, mFittedLines[i].pts.size());
+	cv::imshow("distance sensors", mImage);
+	cv::waitKey(0);
 		}
 	}
 
