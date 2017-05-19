f98ab60ed6f4cb18872387302ddd19fc33580449
alexzhang13 <alex.lx.zhang@gmail.com>
2017-05-18 21:48:41 -0400

    ; fix

--- Software/common/ARobot.cpp
+++ Software/common/ARobot.cpp
@@ -61,7 +61,7 @@ void ARobot::UpdateCellMap(MazeCell *sensor_info, bool black_flag)
 
         /*WALL DATA*/
         printf("walls\n");
-        printf("Walls: N: %d, E: %d, S: %d, W: %d\n", rangeDataList.end()->walls.wallN, rangeDataList.end()->walls.wallE, rangeDataList.end()->walls.wallS, rangeDataList.end()->walls.wallW)
+        printf("Walls: N: %d, E: %d, S: %d, W: %d\n", rangeDataList.end()->walls.wallN, rangeDataList.end()->walls.wallE, rangeDataList.end()->walls.wallS, rangeDataList.end()->walls.wallW);
         if(rangeDataList.end()->walls.wallN == 0) {
             sensor_info->setWallNorth(MazeCell::MWall);
         } else {
