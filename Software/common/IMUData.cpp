ff4b28890ed7f324e1d8cda0f6b0480e493136fa
alexzhang13 <alex.lx.zhang@gmail.com>
2017-05-19 21:24:49 -0400

    remove tstamp stuff

--- Software/common/IMUData.cpp
+++ Software/common/IMUData.cpp
@@ -35,17 +35,17 @@ int IMUData::getTStamp() {
 
 int IMUData::runFilter()
 {
-	if(prev_tstamp != -1) {
+	/*if(prev_tstamp != -1) {
         madgwick.set((float)data.tstamp-(float)prev_tstamp/1000); //take current in parsing timestamp minus data list most updated (previous) and get difference
     } else {
     	madgwick.set(0.026);
-    }
+    }*/
 	madgwick.updateIMU(data.gx/131, data.gy/131, data.gz/131, data.ax/16384, data.ay/16384, data.az/16384);
 	m_pitch = madgwick.getRoll(); //inverted
 	m_roll = madgwick.getPitch(); //inverted
 	m_yaw = madgwick.getYaw();
 
-	printf("%d %d", data.tstamp, prev_tstamp);
+	//printf("%d %d", data.tstamp, prev_tstamp);
 	printf("Timestamp: %i Roll: %f Pitch: %f Yaw: %f\n", data.tstamp,  m_roll, m_pitch, m_yaw);
 	//printf("Timestamp: %i Ax: %f Ay: %f Az: %f\n", data.tstamp, data.ax, data.ay, data.az);
 	return 0;
