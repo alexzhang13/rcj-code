9a1d58c2b5e47b0a4537b9d08678230c1488eee0
Alex <alex.lx.zhang@gmail.com>
2017-12-28 14:44:24 -0500

    Fix IMU drift at start up

--- Software/build/common/_src/processThread.cpp
+++ Software/build/common/_src/processThread.cpp
@@ -7,7 +7,7 @@ void Process_T::run(void){
 
     while(1) {
 		    processed_status = false; 
-        while(myRobot->imuParseList.size() > 0) {
+        while(myRobot->imuParseList.size() > 0 && myRobot->imuCalibrated) {
     	   myRobot->ParseIMU();
            myRobot->ClearIMU();
 		       processed_status = true;
