be1f49d754e5487fdcf9aa120477a7c42a26319f
Alex <alex.lx.zhang@gmail.com>
2017-12-27 23:22:46 -0500

    Test for signal

--- Software/build/common/_src/ARobot.cpp
+++ Software/build/common/_src/ARobot.cpp
@@ -491,7 +491,7 @@ void ARobot::MoveDistance(int distance_mm, BotDir dir) //forward = true
         currState = MOVE;
     }
     printf("Distance: %d\n", distance_mm);
-    TestSignal(); //Test Un
+    //TestSignal(); //Test Un
     WriteCommand(i_command, i_length);
     //TestSignal(); //Test Deux
 }
