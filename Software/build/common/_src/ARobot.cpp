8267f34965727dcd44076421258d80cfd46a02b3
Alex <alex.lx.zhang@gmail.com>
2017-12-28 23:45:32 -0500

    Fix Turn Discrepancy

--- Software/build/common/_src/ARobot.cpp
+++ Software/build/common/_src/ARobot.cpp
@@ -248,16 +248,16 @@ void ARobot::CalcNextTile()
     int32_t dist = (int32_t)sqrt(next_x*next_x + next_y*next_y); //pythagorean
     float angle; //offset angle degrees
     if(currTile.x_tovisit - currTile.x > 0) { //east
-    	angle = atan((float)next_y/(float)next_x)*180.0f/3.1415926535f; //angle to left, should be pos
+    	angle = -atan((float)next_y/(float)next_x)*180.0f/3.1415926535f; //angle to left, should be neg
         nextDir = EAST;
     } else if (currTile.x_tovisit - currTile.x < 0) { //west
-    	angle = atan((float)next_y/(float)next_x)*180.0f/3.1415926535f; //angle to left, should be pos
+    	angle = -atan((float)next_y/(float)next_x)*180.0f/3.1415926535f; //angle to left, should be neg
         nextDir = WEST;
     } else if (currTile.y_tovisit - currTile.y > 0) { //north
-    	angle = -atan((float)next_x/(float)next_y)*180.0f/3.1415926535f; //angle to left, should be pos
+    	angle = atan((float)next_x/(float)next_y)*180.0f/3.1415926535f; //angle to left, should be pos
         nextDir = NORTH;
     } else if (currTile.y_tovisit - currTile.y < 0) { //south
-    	angle = -atan((float)next_x/(float)next_y)*180.0f/3.1415926535f; //angle to left, should be pos
+    	angle = atan((float)next_x/(float)next_y)*180.0f/3.1415926535f; //angle to left, should be pos
         nextDir = SOUTH;
     }
 
@@ -268,7 +268,7 @@ void ARobot::CalcNextTile()
 
     angle *= (turnNext>=0) ? 1 : -1; //determine direction of offset (left >= 0)
 
-    toTurn = turnNext*-90+(int)angle; //turning distance
+    toTurn = turnNext*90+(int)angle; //turning distance
 
 
     //printf("Next_X: %d, Next_Y: %d, Dist: %d, Angle Dif: %f\n", next_x, next_y, dist, angle);
