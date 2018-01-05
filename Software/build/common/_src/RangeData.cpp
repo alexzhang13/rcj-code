ed1359a8d020c1ddf17963cdef7f6a2c2ed4e2d1
Alex <alex.lx.zhang@gmail.com>
2018-01-05 16:00:05 -0500

    IMU Offset Addition

--- Software/build/common/_src/RangeData.cpp
+++ Software/build/common/_src/RangeData.cpp
@@ -7,6 +7,8 @@
 
 #define OFFSET 17.5
 #define PI 3.1415926535
+#define SHORTOFFSET 30.0
+#define LONGOFFSET 55.0
 
 RangeData::RangeData(ARobot *robot) :myRobot(robot)
 {
@@ -42,6 +44,16 @@ void RangeData::storeCommand(char* buf) {
 int RangeData::parseData()
 {
 	sscanf(m_command, "%d %c %d %f %f %f %f", &data.tstamp, &data.id, &data.angle, &data.laserL_a, &data.laserS_a, &data.laserL_b, &data.laserS_b);
+	if(data.laserL_a >= 400) {
+		data.laserL_a -= LONGOFFSET;
+	} else if (data.laserL_b >= 300) {
+		data.laserL_a -= LONGOFFSET;
+	}
+	if(data.laserL_b >= 400) {
+		data.laserL_b -= LONGOFFSET;
+	} else if (data.laserL_b >= 300) {
+		data.laserL_b -= SHORTOFFSET;
+	}
 	return 0;
 }
 
@@ -71,7 +83,7 @@ int RangeData::setAngle() {
 	} else if(avalid_long) {
 		int far = ((int)temp_range[0]/300 + (int)temp_range[2]/300 + 1);
 		angled = acos(min(1.0, (double)(far*300)/(data.laserL_a + data.laserL_b + OFFSET))) * 180 / PI;
-		alpha = max(0.0, (7.62/pow(far-1, 3))*((far*300)/(data.laserL_a + data.laserL_b + OFFSET)-0.786)); //30 degree turn range 0-1
+		alpha = max(0.0, (7.62/(far-1))*((far*300)/(data.laserL_a + data.laserL_b + OFFSET)-0.786)); //30 degree turn range 0-1
 		if(alpha >= 1.3) alpha = 0;
 		else if (alpha >= 1) alpha = 1;
 		//printf("Angle: %f\tAlpha: %f\tValue: %i Distance: %f\n", angled, alpha, far*300, data.laserL_a + data.laserL_b + OFFSET);
@@ -109,6 +121,7 @@ int RangeData::getPosition()
 		temp_range[0] = (data.laserL_a-30.0) * std::abs(cos((3.1415926535*curr_yaw)/180.0)); //-18.0
 		temp_dist = (int)temp_range[0]/300;
 		distance[0] = temp_range[0] - temp_dist*300;
+		if(data.laserL_a >= 900) avalid_long = false;
 	} else {
 		distance[0] = 451.0f; //impossible number for distance[0], as it's %300
 		temp_range[0] = -300;
@@ -118,6 +131,7 @@ int RangeData::getPosition()
 		temp_range[2] = (data.laserL_b+59.25) * std::abs(cos((3.1415926535*curr_yaw)/180.0));
 		temp_dist = (int)temp_range[2]/300;
 		distance[2] = temp_range[2] - temp_dist*300;
+		if(data.laserL_a >= 900) avalid_long = false;
 	} else {
 		distance[2] = 451.0f; //impossible number for distance[2], as it's %300
 		temp_range[2] = -300;
