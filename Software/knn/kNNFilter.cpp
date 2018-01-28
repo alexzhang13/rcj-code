fd8100b787e71c0d7a2946ecefadba390ad5a686
Alex <alex.lx.zhang@gmail.com>
2018-01-28 14:21:48 -0500

    Prints added on kNN

--- Software/knn/kNNFilter.cpp
+++ Software/knn/kNNFilter.cpp
@@ -18,24 +18,24 @@ char kNNFilter::detectVictim(cv::Mat matTestingNumbers) {
 		printf("images.xml is non-existent\n");
 		return('0');
 	}
-	cout << "XML's found." << endl;
+	printf("XML's found.\n");
 
 	fsTrainingImages["images"] >> matTrainingImagesAsFlattenedFloats;           // read images section into Mat training images variable
 	fsTrainingImages.release();
-	cout << "Filled var with image" << endl;		
+	printf("Filled var with image\n");		
 	
 	cv::Ptr<cv::ml::KNearest> kNearest(cv::ml::KNearest::create());            // instantiate the KNN object
 	kNearest->train(matTrainingImagesAsFlattenedFloats, cv::ml::ROW_SAMPLE, matClassificationInts);
-	cout << "KNN Object Instantiated" << endl;
+	printf("KNN Object Instantiated\n");
 
 	cv::cvtColor(matTestingNumbers, matGrayscale, CV_BGR2GRAY);
 	cv::GaussianBlur(matGrayscale, matBlurred, cv::Size(5, 5), 0);
 	cv::adaptiveThreshold(matBlurred, matThresh, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 11, 2);
-	cout << "Masks Initialized" << endl;
+	printf("Masks Initialized\n");
 
 	matThreshCopy = matThresh.clone();              // make a copy of the thresh image, this in necessary b/c findContours modifies the image
 	cv::findContours(matThreshCopy, ptContours, v4iHierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-	cout << "Contours found." << endl;
+	printf("Contours found.\n");
 
 	for (int i = 0; i < ptContours.size(); i++) {               // for each contour
 		kNNFilter contourWithData;                                                    // instantiate a contour with data object
@@ -44,17 +44,17 @@ char kNNFilter::detectVictim(cv::Mat matTestingNumbers) {
 		contourWithData.fltArea = cv::contourArea(contourWithData.ptContour);               // calculate the contour area
 		allContoursWithData.push_back(contourWithData);                                     // add contour with data object to list of all contours with data
 	}
-	cout << "Contours searched." << endl;
+	printf("Contours searched.\n");
 
 	for (int i = 0; i < allContoursWithData.size(); i++) {                      // for all contours
 		if (allContoursWithData[i].checkIfContourIsValid()) {                   // check if valid
 			validContoursWithData.push_back(allContoursWithData[i]);            // if so, append to valid contour list
 		}
 	}
-	cout << "Contour's validated." << endl;
+	printf("Contour's validated.\n");
 	// sort contours from left to right
 	std::sort(validContoursWithData.begin(), validContoursWithData.end(), kNNFilter::sortByBoundingRectXPosition);
-	cout << "Contour's Sorted by X Pos." << endl;
+	printf("Contour's Sorted by X Pos.\n");
 
 	for (int i = 0; i < validContoursWithData.size(); i++) {            // for each contour
 		cv::rectangle(matTestingNumbers, validContoursWithData[i].boundingRect, cv::Scalar(0, 255, 0), 2);
@@ -69,7 +69,7 @@ char kNNFilter::detectVictim(cv::Mat matTestingNumbers) {
 		float fltCurrentChar = (float)matCurrentChar.at<float>(0, 0);
 		strFinalString = strFinalString + char(int(fltCurrentChar));        // append current char to full string
 	}
-	cout << "Contours' ROI checked and converted to string of chars." << endl;
+	printf("Contours' ROI checked and converted to string of chars.\n");
 
 	for(int i = 0; i < strFinalString.size(); i++) { //if anything corresponds correctly, return it? logic flawed but will do...
 		printf("Char: %c\n", strFinalString[i]);
