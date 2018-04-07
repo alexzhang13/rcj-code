8f6ca407585a84444f55c2bd6c00bf269c0d0714
Alex <alex.lx.zhang@gmail.com>
2018-04-06 21:15:07 -0400

    Tester Thread

--- Software/common/testThread.cpp
+++ Software/common/testThread.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 void TestThread::run(void){
+    printf("Test Thread Started...\n");
     sleep(1);
     while(1) {
         myRobot->TestRangeSensors();
-        sleep(0.5);
+        sleep(2);
     }
+    return;
 }
