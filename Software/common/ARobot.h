8826cb41a273415ee3ef62967561811d5ac5c65f
Alex <alex.lx.zhang@gmail.com>
2018-04-17 21:52:31 -0400

    Enhanced For loop error fixed

--- Software/common/ARobot.h
+++ Software/common/ARobot.h
@@ -127,7 +127,7 @@ public:
     /*Inline Functions*/
     float getSTD(int* arr, int avg) {
         float total = 0;
-        for(const int& : arr)
+        for(const int& val: arr)
             total += std::sqrt((float)std::pow(val-avg, 2)/(sizeof(arr)/sizeof(arr[0]));
         return total;
     }
