8398b6f85f5e234087469472b4bbd4b22d37e650
Alex <alex.lx.zhang@gmail.com>
2018-04-22 14:33:10 -0400

    Update Cam

--- Software/common/picamera.cpp
+++ Software/common/picamera.cpp
@@ -51,15 +51,15 @@ bool _PiCamera_::frameCapture(std::string fileName)
 {
     cv::Mat img;
     //capture
-    Camera.grab();
+    m_camera.grab();
     //allocate memory
-    unsigned char *data=new unsigned char[Camera.getImageTypeSize (raspicam::RASPICAM_FORMAT_RGB)];
+    unsigned char *data=new unsigned char[m_camera.getImageTypeSize (raspicam::RASPICAM_FORMAT_RGB)];
     //extract the image in rgb format
-    Camera.retrieve (data, raspicam::RASPICAM_FORMAT_RGB);//get camera image
+    m_camera.retrieve (data, raspicam::RASPICAM_FORMAT_RGB);//get camera image
     //save
     std::ofstream outFile (fileName,std::ios::binary );
-    outFile<<"P6\n" <<Camera.getWidth() << " " << Camera.getHeight() <<" 255\n";
-    outFile.write((char*)data, Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB));
+    outFile<<"P6\n" <<m_camera.getWidth() << " " << m_camera.getHeight() <<" 255\n";
+    outFile.write((char*)data, m_camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB));
     cout<<"Image saved at raspicam_image.ppm"<<endl;
     delete data;
     return true;
