5ebf4bbec9f51480c9e54d12af80ef818424ef9f
Alex <alex.lx.zhang@gmail.com>
2018-04-25 22:57:34 -0400

    Added  Kill for PThreads (NavThread)

--- Software/common/navThread.cpp
+++ Software/common/navThread.cpp
@@ -310,9 +310,13 @@ int NavThread::WayPointNav(ARobot *robot, Navigate2D &nav_rt)
 
 void NavThread::DestroyThread()
 {
+    printf("1");
     myRobot->currState = ARobot::STOP;
+    printf("2");
     myRobot->StopMove();
-    myRobot->picam.close();
+    //myRobot->picam.close();
+    printf("3");
     this->mExitFlag = true;
+    printf("4\n");
 }
 
