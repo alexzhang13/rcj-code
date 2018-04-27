c20e1ca1b0ffd48b0e7d177a4405786e9808824b
Alex <alex.lx.zhang@gmail.com>
2018-04-26 22:14:26 -0400

    Ramp + Black Tile

--- Software/common/navThread.cpp
+++ Software/common/navThread.cpp
@@ -86,9 +86,11 @@ void NavThread::run(void){
             break;
         case 7: //BLACKBACK
             sleep(1);
+            bot_waypts = myRobot->waypts.size();
             myRobot->backingBlack = false;
             nav.getNavigateMaps()->getFloorMap(nav.getCurrentFloorIndex())->setCurCellIndex(myRobot->waypts[bot_waypts-2]); //update "temp curr_cell"
             myRobot->UpdateCellMap(&myRobot->sensor_info, true, false); //sensor_info auto resets in this function call
+            nav.configureCurCell(&myRobot->sensor_info);
             nav.getNavigateMaps()->getFloorMap(nav.getCurrentFloorIndex())->setCurCellIndex(myRobot->waypts[bot_waypts-1]); //reupdate curr_cell
             myRobot->currState = ARobot::PLANNING;
             break;
